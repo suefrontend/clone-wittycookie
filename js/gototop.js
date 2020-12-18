b8fb5b043acaede6fb709ef218a853a832fdea35
Kaya0312 <oyama_kayano@yahoo.co.jp>
2020-12-18 15:05:41 +0900

    Fix go-to-top button JavaScript code

--- js/gototop.js
+++ js/gototop.js
@@ -2,26 +2,26 @@
 // もし、topからXXpx（メイン画像の高さ分）までスクロールしてきたら、activeクラスを付ける
 // もし、topからxxpxまで近づいたら、activeを外す
 
+// .go-to-top をdocument.querySelectorで取得する
+const btn = document.querySelector('.go-to-top');
+
 // .go-to-top の上からの座標を取得
 window.onscroll = function() {
-  var y = window.pageYOffset;
-
-  // .go-to-top をdocument.querySelectorで取得する
-  const btn = document.querySelector('.go-to-top');
-
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  }
-
-  // クリックしたら、ページの一番上へスクロールして戻るようにする
-  btn.addEventListener('click', scrollToTop);
+  const pageYOffset = window.pageYOffset;
 
   if(y > 200) {
     btn.classList.add('active')
   } else {
     btn.classList.remove('active')
   }
+}
+
+// クリックしたら、ページの一番上へスクロールして戻るようにする
+btn.addEventListener('click', scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 }
